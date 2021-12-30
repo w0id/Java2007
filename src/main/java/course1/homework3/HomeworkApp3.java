a1b0aaabbffcd1f952b21e0ebf94de9f914d0e4f
W0iD <xcrypter@gmail.com>
2021-12-30 20:00:18 +0300

    Домашка 3 + 6*, 7*

--- src/main/java/course1/homework3/HomeworkApp3.java
+++ src/main/java/course1/homework3/HomeworkApp3.java
@@ -11,15 +11,18 @@ public class HomeworkApp3 {
           С помощью цикла и условия заменить 0 на 1, 1 на 0;
          */
         reverseArrayItems();
+        System.out.println();
         /*
           2. Задать пустой целочисленный массив длиной 100.
           С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
          */
         hundredItemsArray();
+        System.out.println();
         /*
           3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
          */
         multiplicateArrayItems();
+        System.out.println();
         /*
          * 4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
          * и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно).
@@ -27,11 +30,26 @@ public class HomeworkApp3 {
          * то есть [0][0], [1][1], [2][2], …, [n][n];
          */
         drawCross();
+        System.out.println();
         /*
          * 5. Написать метод, принимающий на вход два аргумента: len и initialValue,
          * и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
          */
         fillArray(10, 7);
+        System.out.println();
+        /*
+         * 6. * Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
+         */
+        maxnminArrayItems();
+        System.out.println();
+        /*
+         * 7. ** Написать метод, в который передается не пустой одномерный целочисленный массив,
+         * метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
+         */
+        Random random = new Random();
+        int[] summArray = random.ints(10,0, 15).toArray();
+        System.out.println(Arrays.toString(summArray));
+        System.out.println(arrayItemsSymmetrySumm(summArray));
     }
 
     public static void reverseArrayItems() {
@@ -76,5 +94,33 @@ public class HomeworkApp3 {
         int[] array = new int[len];
         Arrays.fill(array,initialValue);
         System.out.println(Arrays.toString(array));
+    }
+
+    public  static void maxnminArrayItems() {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("Минимальный элемент: " + Arrays.stream(array).min().getAsInt());
+        System.out.println("Максимальный элемент: " + Arrays.stream(array).max().getAsInt());
+    }
+
+    public static boolean arrayItemsSymmetrySumm(int[] array) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int left = array[leftIndex];
+        int right = array[array.length - 1 - rightIndex];
+        for (int item:array) {
+            if (left > right) {
+                rightIndex++;
+                right += array[array.length - 1 - rightIndex];
+            } else if (left < right){
+                leftIndex++;
+                left += array[leftIndex];
+            } else {
+                return true;
+            }
+            if(leftIndex == array.length - 1 - rightIndex){
+                break;
+            }
+        }
+        return false;
     }
 }
