453ceb843ee37c0b10776431a6cfae7fab814dbb
W0iD <xcrypter@gmail.com>
2022-01-16 23:15:50 +0300

    Домашка 5

---
+++ src/main/java/course1/homework5/Employee.java
@@ -0,0 +1,71 @@
+package course1.homework5;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public Employee(String fullName, String position, String email, String phone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void employeeInfo() {
+        System.out.println("Должность: " + position + "\nФИО: " + fullName + "\nEmail: " + email + "\nТелефон: " + phone + "\nЗарплата: " + salary + "\nВозраст: " + age);
+    }
+}
